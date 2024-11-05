6e799edb5612f145ae20af54770e855d4f5588c8
AlecP <alec.postea26@ycdsbk12.ca>
2024-11-05 10:48:42 -0500

    Update Alec's running game.py

--- Alec's running game.py
+++ Alec's running game.py
@@ -3,14 +3,16 @@ import pygame
 
 pygame.init()
 
-WIDTH = 640
-HEIGHT = 480
+WIDTH = 800
+HEIGHT = 600
 SIZE = (WIDTH, HEIGHT)
 FPS = 60
 screen = pygame.display.set_mode(SIZE)
 clock = pygame.time.Clock(FPS)
 # Colours
 WHITE = (255, 255, 255)
+RED = (250, 0, 0)
+NIGHT_SKY = (46, 68, 130)
 # ---------------------------
 # Initialize global variables
 
@@ -25,14 +27,17 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    
+    # DRAWING
+    screen.fill((NIGHT_SKY)  # always the first drawing command
 
+    # Basic cars
+    pygame.draw.rect(screen, (RED), (
 
   
     # GAME STATE UPDATES
     # All game math and comparisons happen here
 
-    # DRAWING
-    screen.fill((WHITE))  # always the first drawing command
 
     pygame.draw.circle(screen, (0, 0, 255), (circle_x, circle_y), 30)
 
