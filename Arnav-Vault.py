b26aca5206789e4ef5e7343cf0a92b36ed5ad4cd
Arnav Mathur <177362306+IgnoreMeee@users.noreply.github.com>
2024-11-07 11:04:58 -0500

    Update Arnav-Vault.py
    - Added ability to hide (Guard will not see you while hiding)
    - Added vault
    - Represented opening vault progress

--- Arnav-Vault.py
+++ Arnav-Vault.py
@@ -1,6 +1,5 @@
 import pygame
 
-
 pygame.init()
 
 WIDTH = 800
@@ -8,6 +7,8 @@ HEIGHT = 600
 SIZE = (WIDTH, HEIGHT)
 
 screen = pygame.display.set_mode(SIZE)
+pygame.display.set_caption("Vault Crack")
+
 clock = pygame.time.Clock()
 
 # ---------------------------
@@ -22,9 +23,11 @@ player_x_vel = 3
 gravity = 9.1
 
 ground = pygame.Rect(0, HEIGHT - 50, WIDTH, 50)
+
 hiding = False
 
 lockers = []
+vaults = []
 
 # ---------------------------
 
@@ -41,18 +44,14 @@ class Player():
     def move(self):
         self.rect = pygame.Rect(self.x, self.y, 50, 100)
 
-    def hide(self):
-        if self.hiding == False:
-            self.hiding = True
-        else:
-            self.hiding = False
 
-class Gaurd():
+class Guard():
     def __init__(self, x, y, x_vel, direction):
         self.x = x
         self.y = y
         self.x_vel = x_vel
         self.direction = direction
+        self.vision_hitbox = pygame.Rect(0, 0, 0, 0)
         self.aware = False
         self.rect = pygame.Rect(self.x, self.y, 50, 100)
 
@@ -69,17 +68,20 @@ class Gaurd():
 
     def check_collision(self, player):
         if self.rect.colliderect(player):
-            print("Game Over")
+            pass
 
     def vision(self):
         if self.direction == "right":
             pygame.draw.line(screen, (255, 0, 0), (self.x, self.y + 50), (self.x + 400, self.y + 50))
+            self.vision_hitbox = pygame.Rect(self.x, self.y, 400, 100)
         else:
             pygame.draw.line(screen, (255, 0, 0), (self.x, self.y + 50), (self.x - 400, self.y + 50))
+            self.vision_hitbox = pygame.Rect(self.x - 400, self.y, 400, 100)
 
     def see_player(self):
-        self.x *= 5
-        self.aware = True
+        if self.vision_hitbox.colliderect(player):
+            self.aware = True
+            print("Guard sees player")
 
 class Locker():
     def __init__(self, x, y, hiding):
@@ -93,13 +95,35 @@ class Locker():
 
     def hide(self, player):
         if self.rect.colliderect(player):
-            print("You Win")
+            pass
+
+class Vault():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.progress = 0
+        self.max_progress = 1000
+        self.rect = pygame.Rect(self.x, self.y, 100, 100)
+
+    def draw(self):
+        pygame.draw.rect(screen, (0, 0, 0), self.rect)
+
+    def open(self, player):
+        if self.rect.colliderect(player):
+            self.progress += 2
+            if self.progress >= self.max_progress:
+                print("Vault opened")
+                self.progress = 0
 
 player = Player(0, 0)
 
-gaurd1 = Gaurd(0, HEIGHT - 150, 2, "right")
+guard1 = Guard(0, HEIGHT - 150, 2, "right")
+
 locker1 = Locker(200, HEIGHT - 100, hiding)
 lockers.append(locker1)
+
+vault1 = Vault(WIDTH - 100, HEIGHT - 100)
+vaults.append(vault1)
 
 running = True
 while running:
@@ -108,19 +132,31 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_e:
+                if not hiding:
+                    for locker in lockers:
+                        if locker.rect.colliderect(player):
+                            hiding = True
+                else:
+                    hiding = False
+
     keys = pygame.key.get_pressed()
 
     if keys[pygame.K_d]:
-        player.x += player_x_vel
-        player.move()
+        if not hiding:
+            player.x += player_x_vel
+            player.move()
+
     if keys[pygame.K_a]:
-        player.x -= player_x_vel
-        player.move()
-    if keys[pygame.K_e]:
-        if hiding == False:
-            for locker in lockers:
-                if locker.rect.colliderect(player):
-                    locker.hide(player)
+        if not hiding:
+            player.x -= player_x_vel
+            player.move()
+
+    if keys[pygame.K_SPACE]:
+        for vault in vaults:
+            vault.open(player)
+            print(vault.progress)
 
     # GAME STATE UPDATES
     # All game math and comparisons happen here
@@ -128,25 +164,36 @@ while running:
         player.y += gravity
         player.move()
 
-    if gaurd1.x + 50 > 0:
-        gaurd1.move()
-        gaurd1.check_collision(player)
+    if guard1.x + 50 > 0:
+        guard1.move()
+        guard1.check_collision(player)
 
-    locker1.hide(player)
+    if not hiding:
+        guard1.see_player()
+
+    if guard1.aware:
+        if guard1.direction == "right":
+            guard1.x_vel = 5
+        else:
+            guard1.x_vel = -5
 
     # DRAWING
     screen.fill((255, 255, 255))  # always the first drawing command
 
     pygame.draw.rect(screen, (102, 92, 91), ground)
 
-    player.draw()
+    if hiding == False:
+        player.draw()
 
-    locker1.draw()
+    for locker in lockers:
+        locker.draw()
+    
+    for vault in vaults:
+        vault.draw()
 
-    if gaurd1.x + 50 > 0:
-        gaurd1.draw()
-        gaurd1.vision()
-
+    if guard1.x + 50 > 0:
+        guard1.draw()
+        guard1.vision()
 
 
 
