4c18fdc88b3fc52dac2268da0166d3c7783f2916
Alexschoolaccount2 <147199689+Alexschoolaccount2@users.noreply.github.com>
2024-11-07 11:01:01 -0500

    Update Alexander-ATM-Heist.py
    Made the ATM interactable with the player:
    -if player presses the e key near or at an ATM a black box will appear
    -if the player presses e again the black box will disappear
    -player movement is disabled while the box is on the screen.

--- Alexander-ATM-Heist.py
+++ Alexander-ATM-Heist.py
@@ -19,7 +19,13 @@ atm_height = 96
 player = pygame.Rect(player_x,player_y,100,145)
 ground = pygame.Rect(0, 400, 800, 200)
 
+playing = False
+moving = True
+
+
 def draw_atm(x, y):
+    global invis_rect
+    
     #ATM body
     pygame.draw.rect(screen, (50, 50, 150), (x, y+44, atm_width, atm_height))
     
@@ -38,6 +44,8 @@ def draw_atm(x, y):
     #ATM Top piece
     pygame.draw.rect(screen, (20, 20, 90), (x+9, y -17, 62, 13))
 
+
+
     # Keypad
     keypad_x = x + 15
     keypad_y = y + 70
@@ -45,31 +53,49 @@ def draw_atm(x, y):
         for col in range(3):
             pygame.draw.rect(screen, (100, 100, 100), (keypad_x + col * 17, keypad_y + row * 20, 15, 15))
 
+    invis_rect = (x-25,y+44,atm_width+50,30)
+
+def GameBox():
+        pygame.draw.rect(screen,(0,0,0),(148,160,500,300))
+
 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    #Movement
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_e:
+                if player.colliderect(invis_rect):
+                    playing = not playing
+
+
     keys_pressed = pygame.key.get_pressed()
+    if moving == True:
+        if keys_pressed[pygame.K_d]:
+            player_x += 5
 
-    if keys_pressed[pygame.K_d]:
-        player_x += 5
-
-    if keys_pressed[pygame.K_a]:
-        player_x += -5
+        if keys_pressed[pygame.K_a]:
+            player_x += -5
 
     player.x = player_x
 
-    #Art
+
     screen.fill((255, 255, 255))  
     pygame.draw.rect(screen, (80,80,80), ground)
     draw_atm(220,260)
     pygame.draw.rect(screen,(0,0,0),player)
     
+    if playing:
+        GameBox()
+        moving = False
+    if not playing:
+        moving = True
 
     pygame.display.flip()
     clock.tick(60)
     
+
+
 pygame.quit()
