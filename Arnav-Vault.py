87cc2b6daabb49b9e9a89818692fd3417190df0a
Arnav Mathur <177362306+IgnoreMeee@users.noreply.github.com>
2024-11-08 11:05:39 -0500

    Update Arnav-Vault.py
    - Added coin counter in top right
    - Added progress bar on top of lockers when you start opening them
    - Made game close (for now) when guard catches you

--- Arnav-Vault.py
+++ Arnav-Vault.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 pygame.init()
 
@@ -20,14 +21,19 @@ player_width = 50
 player_height = 100
 player_x_vel = 3
 
+money = 0
+
 gravity = 9.1
 
 ground = pygame.Rect(0, HEIGHT - 50, WIDTH, 50)
 
 hiding = False
 
+guards = []
 lockers = []
 vaults = []
+
+font = pygame.font.Font('freesansbold.ttf', 32)
 
 # ---------------------------
 
@@ -72,7 +78,7 @@ class Guard():
 
     def vision(self):
         if self.direction == "right":
-            pygame.draw.line(screen, (255, 0, 0), (self.x, self.y + 50), (self.x + 400, self.y + 50))
+            pygame.draw.line(screen, (255, 0, 0), (self.x + 50, self.y + 50), (self.x + 400, self.y + 50))
             self.vision_hitbox = pygame.Rect(self.x, self.y, 400, 100)
         else:
             pygame.draw.line(screen, (255, 0, 0), (self.x, self.y + 50), (self.x - 400, self.y + 50))
@@ -102,28 +108,30 @@ class Vault():
         self.x = x
         self.y = y
         self.progress = 0
+        self.percent = 0
         self.max_progress = 1000
+        self.max_bar = self.x + 100
         self.rect = pygame.Rect(self.x, self.y, 100, 100)
 
     def draw(self):
-        pygame.draw.rect(screen, (0, 0, 0), self.rect)
+        pygame.draw.rect(screen, (100, 100, 100), self.rect)
 
     def open(self, player):
         if self.rect.colliderect(player):
             self.progress += 2
-            if self.progress >= self.max_progress:
-                print("Vault opened")
-                self.progress = 0
 
 player = Player(0, 0)
 
 guard1 = Guard(0, HEIGHT - 150, 2, "right")
+guards.append(guard1)
 
 locker1 = Locker(200, HEIGHT - 100, hiding)
 lockers.append(locker1)
 
-vault1 = Vault(WIDTH - 100, HEIGHT - 100)
+vault1 = Vault(WIDTH - 100, HEIGHT - 150)
 vaults.append(vault1)
+vault2 = Vault(WIDTH - 300, HEIGHT - 150)
+vaults.append(vault2)
 
 running = True
 while running:
@@ -156,7 +164,6 @@ while running:
     if keys[pygame.K_SPACE]:
         for vault in vaults:
             vault.open(player)
-            print(vault.progress)
 
     # GAME STATE UPDATES
     # All game math and comparisons happen here
@@ -164,36 +171,59 @@ while running:
         player.y += gravity
         player.move()
 
-    if guard1.x + 50 > 0:
-        guard1.move()
-        guard1.check_collision(player)
+    for guard in guards:
+        if guard.x + 50 > 0:
+            guard.move()
+            guard.check_collision(player)
 
     if not hiding:
-        guard1.see_player()
+        for guard in guards:
+            guard.see_player()
+            if guard.rect.colliderect(player):
+                running = False
 
-    if guard1.aware:
-        if guard1.direction == "right":
-            guard1.x_vel = 5
-        else:
-            guard1.x_vel = -5
+    for guard in guards:
+        if guard.aware:
+            if guard.direction == "right":
+                guard.x_vel = 5
+            else:
+                guard.x_vel = -5
 
     # DRAWING
     screen.fill((255, 255, 255))  # always the first drawing command
 
     pygame.draw.rect(screen, (102, 92, 91), ground)
 
-    if hiding == False:
-        player.draw()
-
     for locker in lockers:
         locker.draw()
     
     for vault in vaults:
         vault.draw()
+        if vault.progress > 0 and vault.progress < vault.max_progress:
+            vault.percent = (vault.progress / vault.max_progress) * 100
+            pygame.draw.rect(screen, (0, 255, 0), (vault.x, vault.y - 20, vault.percent, 10))
 
-    if guard1.x + 50 > 0:
-        guard1.draw()
-        guard1.vision()
+        if vault.progress >= vault.max_progress:
+            vaults.remove(vault)
+            money += 100
+    
+    text = font.render('Money: ' + str(money), True, (0, 0, 0))
+
+    for guard in guards:
+        if guard.x + 50 > 0:
+            guard.draw()
+            guard.vision()
+        else:
+            guards.remove(guard)
+
+    if random.randint(0, 500) == 1 and len(guards) < 1:
+        guard2 = Guard(0, HEIGHT - 150, 2, "right")
+        guards.append(guard2)
+
+    if hiding == False:
+        player.draw()
+
+    screen.blit(text, (0, 0))
 
 
 
